843001fafcc8e09b4f93b8e4cd442e7dd220580c
kusbah <abdkusbah2@gmail.com>
2025-01-29 12:32:42 +0200

    add the title componant

---
+++ src/Page/Title.tsx
@@ -0,0 +1,36 @@
+import styles from "./Title.module.css";
+import { NodeData } from "../utils/types";
+import { useEffect, useRef } from "react";
+import { nanoid } from "nanoid";
+
+type TitleProps = {
+  title: string;
+  changePageTitle(title: string): void;
+  addNode(node: NodeData, index: number): void;
+};
+
+export const Title = ({ title, changePageTitle, addNode }: TitleProps) => {
+  const headerRef = useRef<HTMLHeadElement>(null);
+  useEffect(() => {
+    const isFocused = document.activeElement == headerRef.current;
+    if (!isFocused && headerRef.current) {
+      headerRef.current.textContent = title;
+    }
+  }, [title]);
+  return (
+    <div className={styles.container}>
+      <h1
+        className={styles.title}
+        contentEditable
+        suppressContentEditableWarning
+        onInput={(e) => changePageTitle(e.currentTarget.textContent || "")}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+            addNode({ type: "text", id: nanoid(), value: "" }, 0);
+          }
+        }}
+      />
+    </div>
+  );
+};
